177dc5069419dba1efce31853fb58ba4cc1986a2
Kurt Brown <128055241+klbrown13@users.noreply.github.com>
2023-08-20 18:47:48 -0400

    Add files via upload

---
+++ Initials.java
@@ -0,0 +1,14 @@
+
+public class Initials {
+	public static void main(String[] args) {
+		System.out.println("K     K       L            BBBBBB    !!");
+		System.out.println("K    K        L            B     B   !!");
+		System.out.println("K   K         L            B      B  !!");
+		System.out.println("K  K          L            B     B   !!");
+		System.out.println("KKK           L            BBBBBB    !!");
+		System.out.println("K  K          L            B     B   !!");
+		System.out.println("K   K         L            B      B  !!");
+		System.out.println("K    K        L            B     B   !!");
+		System.out.println("K     K       LLLLLLL      BBBBBB    !!");
+	}
+}
